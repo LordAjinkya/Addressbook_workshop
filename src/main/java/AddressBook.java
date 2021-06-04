5e5a56a3f3c271bf9f86f7500c2d2407b756cf25
@LordAjinkya <ajinkyanarale007@gmail.com>
2021-06-04 10:21:33 +0530

    Added:code to add multiple contacts to the address book

--- src/main/java/AddressBook.java
+++ src/main/java/AddressBook.java
@@ -1,95 +1,209 @@
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
-/*@Description-Creating a Address Book.
- * Welcome to address book.
- * Contacts are created.
- * Add a new contacts into a address book.
- * Edit the contact details in address book.
- * Check a first name is match in address book then change details of that person. */
 public class AddressBook {
-    public List<Contacts> contactList;
 
-    /* @Description - Add new contacts to the address book  */
+    public ArrayList<Contact> contactlist = new ArrayList<Contact>();
 
-    public List<Contacts> addContacts(Contacts contact) {
-        contactList = new ArrayList<>();
-        try {
-            contactList.add(contact);
-            return contactList;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return contactList;
+    /* this method is used to display welcom message
+     */
+    public void message(){
+        System.out.println("Welcome to Address book");
     }
-    /*Edit the contact details in address book.
-     * Check a first name is match in address book then change details of that person.*/
-    public Contacts editContacts(List<Contacts> contactList, String firstName, String name, String editDetails) {
-        try {
-            for(Contacts contact : contactList){
-                if(contact.firstName.equals(name)) {
-                    switch (name) {
-                        case "firstName":
-                            contact.firstName = editDetails;
-                            break;
-                        case "lastName":
-                            contact.lastName = editDetails;
-                            break;
-                        case "address":
-                            contact.address = editDetails;
-                            break;
-                        case "city":
-                            contact.city = editDetails;
-                            break;
-                        case "state":
-                            contact.state = editDetails;
-                            break;
-                        case "zip":
-                            contact.zip = Integer.parseInt(editDetails);
-                            break;
-                        case "phoneNumber":
-                            contact.phoneNumber = Long.parseLong(editDetails);
-                            break;
-                        case "email":
-                            contact.email = editDetails;
-                            break;
+
+    /* This method is used to add the contact, if already exists in the contact book
+    not adding else adding
+    @param takes contact input
+    @return boolean value true if added else false
+    */
+    public boolean addNewContact(Contact contact) {
+        int bit_add = 0;
+        if (contactlist.size() > 0) {
+            boolean checkExists=this.checkContactExists(contact);
+            if(checkExists==true)
+                return false;
+        }
+        contactlist.add(contact);
+        return true;
+
+    }
+
+    /*This method is used to return Arraylist of contacts
+     */
+    public ArrayList<Contact> getContactlist(){
+        return contactlist;
+    }
+
+    public static void main(String[] args) {
+        AddressBook addressBook=new AddressBook();
+        addressBook.updateMainMethod();
+    }
+    public void updateMainMethod(){
+        AddressBook addressBook=new AddressBook();
+        // addressBook.addNewContactFromConsole();
+        // addressBook=new AddressBook();
+        Contact contact= new Contact("Manu","Kv","Bengaluru","Karnataka",560076
+                ,966339366,"manukvshetty@gmail.com");
+        Contact contact1=new Contact("Srinivas","Kv","Bengaluru","Karnataka",560076
+                ,526157122,"srinivas@gmail.com");
+        addressBook.addNewContact(contact);
+        addressBook.addNewContact(contact1);
+        Scanner consoleInputReader=new Scanner(System.in);
+        System.out.println("Enter the FirstName of the contact to edit");
+        String firstName=consoleInputReader.next();
+        System.out.println("Enter the LastName of the contact to edit");
+        String lastName=consoleInputReader.next();
+        Contact oldContact=addressBook.getContact(firstName,lastName);
+        if(oldContact==null) {
+            System.out.println("contact doesn't exist");
+            return;
+        }
+        Contact newContact =new Contact(consoleInputReader);
+        addressBook.updateContact(oldContact,newContact);
+    }
+
+    /*This method is used to take input form console and add new contact to AddressBook
+    @return boolen value
+     */
+    public  boolean addNewContactFromConsole(){
+        Scanner consoleInputReader=new Scanner(System.in);
+        this.contactlist.add(new Contact(consoleInputReader));
+        return true;
+    }
+
+    /* This method is used to update the contact,only  if exists in the contact book
+    @param takes String oldname and newname as an input
+    @return boolean value true if updated else false
+    */
+    public boolean updateContact(Contact oldcontact, Contact newContact) {
+        boolean check = checkContactExists(oldcontact);
+        if (check == true) {
+            if (contactlist.size() > 0) {
+                for (int i = 0; i < contactlist.size(); i++) {
+                    if (contactlist.get(i).equals(oldcontact)) {
+                        contactlist.remove(i);
+                        contactlist.add(i, newContact);
+                        return true;
                     }
                 }
-                return contact;
             }
-        } catch (Exception e) {
-            System.out.println("Detail not found " + e);
-            e.printStackTrace();
+            else
+            {
+                System.out.println("name not available to edit");
+                return false;
+            }
+        }
+        System.out.println("No contacts");
+        return false;
+    }
+
+    /* This method is used to search the contact,only  if exists in the contact book
+    @param takes contacts
+    @return boolean value true if present else false
+     */
+    public boolean checkContactExists(Contact contact) {
+        if (contactlist.size() > 0 && contact!=null) {
+            int i = 0;
+            while (i < contactlist.size()) {
+                Contact contactPresent = contactlist.get(i);
+                if (contact.equals(contactPresent)) {
+                    return true;
+                }
+                i++;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    /* This method is used to print the contacts
+     */
+    public void printContactList() {
+        System.out.println("You have " + contactlist.size() + " contacts in your list");
+        for (int i = 0; i < contactlist.size(); i++) {
+            Contact contact = contactlist.get(i);
+            System.out.println(contact);
+            System.out.println("================================");
+        }
+    }
+
+    /* This method is used to search the contact,only  if exists in the contact book
+    @param takes FirstName and LastName
+    @return contact
+     */
+    public Contact getContact(String firstName, String lastName) {
+        if (contactlist.size() > 0) {
+            int i = 0;
+            while (i < contactlist.size()) {
+                Contact contactPresent = contactlist.get(i);
+                if ((firstName+lastName).equals(contactPresent.firstName+contactPresent.lastName)) {
+                    System.out.println("Contact Exists");
+                    return contactPresent;
+                }
+                i++;
+            }
+            return null;
         }
         return null;
     }
-    /*Delete a contact using person first name ina address book. */
-    public List<Contacts> deleteContacts(List<Contacts> contactsList, String firstName) {
-        try {
-            for (Contacts contact : contactsList) {
-                if (contact.firstName.equals(firstName)) {
-                    contactsList.remove(contactsList.indexOf(contact));
-                    System.out.println("Enter a first name of person: " +firstName);
-                    break;
-                }
-            }
-            return contactsList;
-        } catch (Exception e) {
-            System.out.println("Entered Name was not found in address book :" +e);
-            e.printStackTrace();
-        }
-        return contactsList;
+
+    /* This method is used to search the contact,only  if exists in the contact book
+    @param takes contacts
+    @return contact value true if present else null
+     */
+    public Contact getContact(Contact newContact) {
+        return contactlist.stream().filter(contact -> contact.equals(newContact))
+                .findAny()
+                .orElse(null);
     }
-    /*Add Multiple contacts in address book. */
-    public List<Contacts> addMultipleContactsList(List<Contacts> contactDataList) {
-        try {
-            for (Contacts contacts : contactDataList) {
-                addContacts(contacts);
+
+    /* This method is used to delete the contact,only  if exists in the contact book
+    @param takes FirstName and LastName
+    @return true if contact deleted else false
+     */
+    public boolean deleteContact(String firstName, String lastName) {
+        if (contactlist.size() > 0) {
+            int i = 0;
+            while (i < contactlist.size()) {
+                Contact contactPresent = contactlist.get(i);
+                if ((firstName+lastName).equals(contactPresent.firstName+contactPresent.lastName)) {
+                    contactlist.remove(i);
+                    return true;
+                }
+                i++;
             }
-            return contactList;
-        } catch (Exception e) {
-            e.printStackTrace();
+            return false;
         }
-        return contactDataList;
+        return false;
+    }
+
+    /* This method is used to add multiple contacts
+    @param takes array of contacts
+    @return number of contacts added
+    */
+    public int addMultipleContacts(Contact[] contactsArray) {
+        int contactsAdded=0;
+        if(contactsArray.length>0){
+            int i=0;
+            while(i<contactsArray.length){
+                boolean checkadded=this.addNewContact(contactsArray[i]);
+                if(checkadded==true)
+                    contactsAdded++;
+                i++;
+            }
+        }
+        return contactsAdded;
+    }
+
+    /* This method is used to get size of AddessBook
+    return size of contactList
+     */
+    public int getSizeOfAddressBook(){
+        return contactlist.size();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
     }
 }
